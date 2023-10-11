1a92bc4d5a01459b0347f259232906800b3ac0f2
Donald Ajaps <95923018+adobki@users.noreply.github.com>
2023-10-11 12:01:49 +0100

    Fix 102-type_checking.py in 0x00

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python3
 """ This module showcases complex annotations in Python """
-from typing import Mapping, Any, Union, TypeVar, TypeAlias
-
-Tuple: TypeAlias = tuple
+from typing import List, Tuple
 
 
-def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
-    """ Duck typing for complex annotations in Python"""
-    zoomed_in: Tuple = [
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """ Duck typing for complex annotations in Python """
+    zoomed_in = [
         item for item in lst
         for i in range(factor)
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(array, 3)
 
 
 if __name__ == '__main__':
