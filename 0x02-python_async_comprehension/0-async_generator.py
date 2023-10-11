04c1d4afcaf9fbdace2d6d7caa8b689fce517002
Donald Ajaps <95923018+adobki@users.noreply.github.com>
2023-10-11 11:26:09 +0100

    Fix tasks in 0x02.Python: Async Comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """ This module showcases working with asynchronous coroutines in Python """
 import asyncio
 from random import uniform
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator:
+async def async_generator() -> Generator[float, None, None]:
     """ A basic async generator that completes execution in ten seconds """
     for _ in range(10):
         await asyncio.sleep(1)
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """ This module showcases working with asynchronous coroutines in Python """
 import asyncio
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list:
+async def async_comprehension() -> List[float]:
     """ Gathers results from an async generator """
     return [num async for num in async_generator()]
 
